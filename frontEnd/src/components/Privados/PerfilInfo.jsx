0b63f9c334a2a6ec634365540bdc0eb7b5404691
manutara7 <pcbackrmartinez@gmail.com>
2026-01-22 11:58:35 -0300

    commit PerfilInfo.jsx componente Privado frontEnd agregado

---
+++ frontEnd/src/components/Privados/PerfilInfo.jsx
@@ -0,0 +1,48 @@
+//src/components/Privados/PerfilInfo.jsx
+
+export default function PerfilInfo({ perfil, setPerfil }) {
+
+  const handleChange = (e) => {
+    setPerfil({
+      ...perfil,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log("Perfil actualizado:", perfil);
+    alert("Perfil actualizado correctamente ✅");
+  };
+
+  return (
+    <section className="card">
+      <h2>Mi Perfil</h2>
+
+      <input
+        name="nombre"
+        placeholder="Nombre"
+        value={perfil.nombre}
+        onChange={handleChange}
+      />
+
+      <input
+        name="apellido"
+        placeholder="Apellido"
+        value={perfil.apellido}
+        onChange={handleChange}
+      />
+
+      <input
+        name="correo"
+        placeholder="Correo"
+        value={perfil.correo}
+        onChange={handleChange}
+      />
+
+      <button className="btn" onClick={handleSubmit}>
+        Guardar cambios
+      </button>
+    </section>
+  );
+}
+
