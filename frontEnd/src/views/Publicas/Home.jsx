760b7e892c04c9a97589fd8d1e94d4546d3c53c3
manutara7 <pcbackrmartinez@gmail.com>
2026-01-22 12:03:33 -0300

    commit Home.jsx  componente Publico frontEnd agregado

---
+++ frontEnd/src/views/Publicas/Home.jsx
@@ -0,0 +1,18 @@
+// src/views/Publicas/Home.jsx
+
+import Header from "../../components/Publicos/Header";
+import Galleria from "../../components/Publicos/Galleria";
+import Footer from "../../components/Publicos/Footer";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Galleria />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
